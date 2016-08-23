69ac8d43b2445151c010d329ee1a9814008723ae
Your Name <arssonist@yahoo.com>
2016-08-22 20:29:32 -0400

    portfolio in proress

--- portfolio.rb
+++ portfolio.rb
@@ -4,8 +4,15 @@ require 'sinatra'
 
 
 get '/' do
+  loop do
   @my_app_name = "Chris: Web Designer Extrada-not"
   @time = Time.new
   @city = "Toronto"
   erb :index
+end
+end
+
+#always a slash in front
+get '/portfolio/about' do
+  erb :about
 end
