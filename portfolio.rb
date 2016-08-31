875b0935eca671d89d59ed30ea17b22628a8c302
Your Name <arssonist@yahoo.com>
2016-08-31 10:03:14 -0400

    portfolio add contact page

--- portfolio.rb
+++ portfolio.rb
@@ -32,3 +32,59 @@ end
 get '/portfolio/other_links' do
   erb :work
 end
+
+
+# Exaple ROUTES
+# #always a slash in front
+# get '/portfolio/about' do
+#   erb :about
+# end
+#
+# get '/portfolio/work' do
+#   erb :work
+# end
+#
+# get 'portfolio/resume' do
+#   erb :work
+# end
+#
+# get '/portfolio/blog' do
+#   erb :work
+# end
+#
+# get '/portfolio/other_links' do
+#   erb :work
+# end
+#
+#
+# get '/' do
+#   .. show something ..
+# end
+#
+# post '/' do
+#   .. create something ..
+# end
+#
+# put '/' do
+#   .. replace something ..
+# end
+#
+# patch '/' do
+#   .. modify something ..
+# end
+#
+# delete '/' do
+#   .. annihilate something ..
+# end
+#
+# options '/' do
+#   .. appease something ..
+# end
+#
+# link '/' do
+#   .. affiliate something ..
+# end
+#
+# unlink '/' do
+#   .. separate something ..
+# end
